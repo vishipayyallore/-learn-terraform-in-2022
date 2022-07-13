1da20a1193e5083fb1717595942f3b3af3206a65
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-07-13 19:21:12 +0530

    Update main.tf

--- Basics/lifecycledemos/main.tf
+++ Basics/lifecycledemos/main.tf
@@ -2,11 +2,24 @@
     Description: This is a simple example of a Terraform lifecycle demo.
 */
 
+terraform {
+  required_version = ">= 1.2.4"
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.2.3"
+    }
+  }
+}
+
+provider "local" {
+  # Configuration options
+}
+
 resource "local_file" "file" {
   filename        = var.filename
   file_permission = var.permission
   content         = random_string.string.id # Implicit Dependencies
-
 }
 
 resource "random_string" "string" {
