8c8b50b02f54e32b058b25d5acd9ca9cfe376165
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-07-25 13:53:56 +0530

    Initial Version - Import Demo

---
+++ AWS/VPC/Demo2Import/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.2.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.23.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-west-2" # Oregon
+}
+
+resource "aws_vpc" "vpc_for_web" {
+  cidr_block           = "192.168.100.0/24"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "VPC for Web"
+  }
+}
