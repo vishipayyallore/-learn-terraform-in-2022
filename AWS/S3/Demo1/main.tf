b9c748ee5c794276c92d6b7a5c6ae1ea56aa8b4e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-07-27 10:18:43 +0530

    Creating S3 Buckets

---
+++ AWS/S3/Demo1/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_version = ">= 1.2.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.23.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = var.region
+}
+
+resource "aws_s3_bucket" "blog" {
+  bucket = "${var.blog_bucket_subdomain}.${var.root_domain}"
+}
+
+resource "aws_s3_bucket_acl" "blog_bucket_acl" {
+  bucket = aws_s3_bucket.blog.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "read_access_policy" {
+  bucket = aws_s3_bucket.blog.id
+  policy = <<POLICY
+          {
+              "Version": "2012-10-17",
+              "Statement": [
+                  {
+                      "Sid": "PublicReadGetObject",
+                      "Effect": "Allow",
+                      "Principal": "*",
+                      "Action": [
+                          "s3:GetObject"
+                      ],
+                      "Resource": [
+                          "arn:aws:s3:::${aws_s3_bucket.blog.id}/*"
+                      ]
+                  }
+              ]
+          }
+POLICY
+}
---
+++ AWS/S3/Demo1/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "us-west-2" # Oregon
+}
+
+variable "root_domain" {
+  default = "s3-bucket-dev-001"
+}
+
+variable "blog_bucket_subdomain" {
+  default = "blog"
+}
+
