d84f69785b75ef535b0d44f32316b286d3c2ab4c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-08-05 12:29:47 +0530

    Initial Version

---
+++ AWS/EC2/A2-TwoSubnetsOneEC2/a1-variables.tf
@@ -0,0 +1,39 @@
+variable "region" {
+  type    = string
+  default = "us-west-2" # Oregon
+}
+
+variable "vpc_cidr_block" {
+  type    = string
+  default = "192.168.100.0/24"
+}
+
+variable "fronentend_subnet_cidr_block" {
+  type    = string
+  default = "192.168.100.0/25"
+}
+
+variable "apig_subnet_cidr_block" {
+  type    = string
+  default = "192.168.100.128/25"
+}
+
+variable "availability_zone_1" {
+  type    = string
+  default = "us-west-2a"
+}
+
+variable "availability_zone_2" {
+  type    = string
+  default = "us-west-2b"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-0528a5175983e7f28"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
---
+++ AWS/EC2/A2-TwoSubnetsOneEC2/a3-outputs.tf
@@ -0,0 +1,3 @@
+output "web_server" {
+  value = aws_instance.web_server
+}
